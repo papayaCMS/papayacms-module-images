71fdb811ed41b96fd97757d0f6d8ee574e777a2f
Thomas Weinert <weinert@dimensional.de>
2014-05-02 15:18:08 +0200

    # Initial Commit, separate module from cms/framework repository

---
+++ src/_classmap.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+  'dyn_image_buttons' => '/dyn_image_buttons.php',
+  'image_buttons' => '/image_buttons.php',
+  'image_colored' => '/image_colored.php',
+  'image_progressbar' => '/image_progressbar.php',
+  'image_ratio' => '/image_ratio.php',
+);
